ad987161876abe6df60f8f8452efb6460e1f6451
carlosnachon-eng <carlos.nachon@emporioinmobiliario.mx>
2026-06-13 15:02:52 -0600

    Update cierres.js

--- pages/cierres.js
+++ pages/cierres.js
@@ -9,7 +9,7 @@ const MESES = ["", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
   "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 const VENDEDORES = ["Carlos", "Ivonne", "Rubi", "Miguel", "Ari", "Andrea",
-  "Guillermo", "Rosario", "Angelica", "Fabiola", "Majo", "Ivan", "Oficina", "Direccion", "Otro"];
+  "Guillermo", "Rosario", "Angelica", "Fabiola", "Majo", "Ivan", "Amanda", "Oficina", "Direccion", "Otro"];
 
 const META_GERENTE = 380000;
 const PCT_ALTO = 0.05;
